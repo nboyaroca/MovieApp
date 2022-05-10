e56122720c8848ac6b47bceddc7e6a57e5d1ae61
[nboyaroca] <[nboyaroca@gmail.com]>
2022-05-10 11:44:18 +0200

    init commit

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,20 @@
+import { Component } from "react";
+
+export class MovieList extends Component {
+    constructor() {
+        super() 
+        this.state={
+            movies: [
+                {
+
+                },
+                {
+
+                },
+            ]
+        }
+    } 
+    render() {
+        return <h1>Hello From Component</h1>
+    }
+}
