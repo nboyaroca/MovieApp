2f856bd2f8c78810b4bf3f690c7e36d5a6bf47d3
[nboyaroca] <[nboyaroca@gmail.com]>
2022-06-01 09:44:33 +0200

    links in test

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import HomePage from './pages/home/HomePage'
 
 test('renders learn react link', () => {
   expect(1).toBe(1);
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { MovieList } from '../../components/cardS/MovieList'
 import NavBar from '../../components/navbar/NavBar'
 
+
 export default function HomePage() {
 
     return (
