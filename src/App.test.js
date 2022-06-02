699c909cb0b23a0594f2afe87149fcc2f1bd3e14
[nboyaroca] <[nboyaroca@gmail.com]>
2022-06-02 09:40:44 +0200

    ready to develop branch

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,4 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import HomePage from './pages/home/HomePage'
+
 
 test('renders learn react link', () => {
   expect(1).toBe(1);
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -3,13 +3,16 @@ import { MovieList } from '../../components/cardS/MovieList'
 import NavBar from '../../components/navbar/NavBar'
 
 
+
+
+
 export default function HomePage() {
 
     return (
       <>
         <NavBar/>
        
-          <MovieList />
+        <MovieList />
        
         
                
