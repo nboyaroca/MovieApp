226471810c8b149dd0354576bb234cc91951f8e5
[nboyaroca] <[nboyaroca@gmail.com]>
2022-06-02 11:03:59 +0200

    testing 4

--- src/application/Router.jsx
+++ src/application/Router.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from '../App';
-import Home from '../pages/home/HomePage';
 import MoviePage from '../pages/movies/MoviePage';
 
 export default function Router() {
@@ -9,9 +8,8 @@ export default function Router() {
     <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
             <Route path="/" element={<App />}/>
-            <Route path="/home" element={<Home />}/>
             <Route path="/movie/:id" element={<MoviePage />}/>
         </Routes>
-    </BrowserRouter>      
+    </BrowserRouter>
   )
 }
